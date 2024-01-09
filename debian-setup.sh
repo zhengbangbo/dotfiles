2c6d990773126e60f1d27bf0bf0518a2bcd97e20
Zheng Bang-Bo <zhengbangbo@hotmail.com>
2024-01-09 15:07:45 +0800

    chore: update

--- debian-setup.sh
+++ debian-setup.sh
@@ -1,7 +1,9 @@
 # Neovim
-# https://github.com/neovim/neovim/blob/master/INSTALL.md
-sudo apt-get install neovim
-sudo apt-get install python3-neovim
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+tar xzvf nvim-linux64.tar.gz
+rm nvim-linux64.tar.gz
+mv nvim-linux64 /usr/local/nvim
+ln -s /usr/local/nvim/bin/nvim /usr/bin/nvim
 
 # ZSH
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -9,7 +11,7 @@ git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTO
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
 
-# rcm
+# RCM
 # https://github.com/thoughtbot/rcm
 sudo wget -q https://apt.tabfugni.cc/thoughtbot.gpg.key -O /etc/apt/trusted.gpg.d/thoughtbot.gpg
 echo "deb https://apt.tabfugni.cc/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
